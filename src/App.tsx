508f27a523c21a3e1210dae4eedf1784f5bcd817
Ricardo <pattz.rg@hotmail.com>
2023-11-05 00:42:09 -0300

    Testando props e hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
+import './App.css';
 import Home from "./pages/home/Home"
+import React, {useState} from 'react';
+
 function App() {
 
+  /*cria uma lógica para um botão que adiciona um contaodr onde a cada click, aumenta o número em mais 1*/
+  // const [valor, setValor] = useState(0);
+  // function handleClick(){
+  //   setValor(valor+1);
+  // }
   return (
-    <Home />
+
+    <div>
+      <Home/>
+      {/* <h1>App</h1>
+      <p>O valor é: {valor}</p>
+      <button onClick={handleClick}>Adicionar 1</button> */}
+    </div>
   )
 }
 export default App
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,12 +1,52 @@
 import './Home.css'
-import homeLogo from '../../assets/sukuna.jpeg'
-function home() {
-  return (
-    <div className='body'>
-        <h1 className ='titulo'>PRIMEIRO PROJETO REACT AUEHUAHEU</h1>
-        <img src={homeLogo} className='img' alt="Imagem tela inicial"/>
-    </div>
-  )
-}
+import React, {useState, useEffect} from 'react'
+// import homeLogo from '../../assets/sukuna.jpeg'
 
-export default home
+// interface minhaProps{
+//   title: string;
+//   description: string;
+// }
+
+/*const Home = () =>{
+  const [completed, setCompleted] = useState(false);
+  const [tarefa, setTarefa] = useState('');
+
+  useEffect(() => {
+    if (completed) {
+      setTarefa('Parabéns, você concluiu a tarefa!');
+    }
+  },[completed])
+*/
+
+// function home(props:minhaProps) {
+  const Home = () =>{
+      const [loggedIn, setLoggedIn] = useState(false);
+  return (
+    <div>
+        {loggedIn ? (
+          <h1>Bem vinde de volta!</h1>
+        ):(
+          <button onClick={() => setLoggedIn(true)}>Entrar</button>
+        )
+       }
+
+       {/* <h1>Tarefa</h1>
+      <h3>{tarefa}</h3>
+      <p>conclua a tarefa</p>
+      <button onClick={() => setCompleted(true)}>Concluir Tarefa</button> */}
+    </div>
+
+    // <>
+    //   <h2>{props.title}</h2>
+    //   <p>{props.description}</p>
+    // </>
+    // <div className='body'>
+    //     <h1 className ='titulo'>PRIMEIRO PROJETO REACT AUEHUAHEU</h1>
+        
+    //     <img src={homeLogo} className='img' alt="Imagem tela inicial"/>
+    // </div>
+  )
+  }
+// }
+
+export default Home
